176d6212d5cc7012618e9b5ffff41d52501d30a6
Alex19010 <sacha2005d@gmail.com>
2024-03-29 07:46:47 +0300

    some

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,33 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['publish']),
+        ]
 
     def publish(self):
         self.published_date = timezone.now()
@@ -31,3 +50,5 @@ class Student(models.Model):
 
     def __str__(self):
         return self.student_name
+
+
